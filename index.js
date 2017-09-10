7bd83e1650d8d6e6b8b436c5367c8cd27c4fcb3c
Subhajit Sahu <wolfram77@gmail.com>
2017-09-11 03:42:30 +0530

    :two_men_holding_hands: remove last 2 chars

--- index.js
+++ index.js
@@ -78,10 +78,7 @@ module.exports = function HerokuAddonPool(id, app, opt) {
       `~/heroku config -s -a ${app} | grep ^${key}`,
       (err, stdout) => {
         const r = stdout.toString();
-        console.log(`r=${r}`);
-        const value = r.substring(r.indexOf('=')+2, r.length-1);
-        console.log(`value=${value}`);
-        fres(supply.get(key).value = value);
+        fres(supply.get(key).value = r.substring(r.indexOf('=')+2, r.length-2));
       }
     ));
   };
